b6a77139b20779ddb4bb9456a0822708f71b491b
Mohamed Aziz Tousli <mohamedaziztousli@gmail.com>
2019-03-13 07:31:01 +0100

    Create Week 4.ino

---
+++ The Arduino Platform and C Programming/Assignments/Week 4.ino
@@ -0,0 +1,14 @@
+void setup()
+{  
+pinMode(13,OUTPUT);  
+Serial.begin(9600);
+}
+void loop()
+{ 
+if(Serial.available() > 0)      
+{      
+char data = Serial.read();                     
+if(data == '0') digitalWrite(13,0);        
+else if (data == '1') digitalWrite(13,1); 
+}
+}
